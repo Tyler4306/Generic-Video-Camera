be7b4486b9407443b8be3a1e26774aeb511fdc4f
TYLER CLAY <tyler4306@gmail.com>
2017-12-25 02:01:24 -0600

    fzxgn

---
+++ devicetypes/tyler4306/ghpiug.src/ghpiug.groovy
@@ -0,0 +1,112 @@
+/**
+ *  ghpiug
+ *
+ *  Copyright 2017 TYLER CLAY
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "ghpiug", namespace: "Tyler4306", author: "TYLER CLAY") {
+		capability "Alarm"
+		capability "Alarm System"
+		capability "Buffered Video Capture"
+		capability "Garage Door Control"
+		capability "Security System"
+		capability "Sensor"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'alarm' attribute
+	// TODO: handle 'alarmSystemStatus' attribute
+	// TODO: handle 'clip' attribute
+	// TODO: handle 'door' attribute
+	// TODO: handle 'securitySystemStatus' attribute
+	// TODO: handle 'alarm' attribute
+
+}
+
+// handle commands
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def strobe() {
+	log.debug "Executing 'strobe'"
+	// TODO: handle 'strobe' command
+}
+
+def siren() {
+	log.debug "Executing 'siren'"
+	// TODO: handle 'siren' command
+}
+
+def both() {
+	log.debug "Executing 'both'"
+	// TODO: handle 'both' command
+}
+
+def sendEvent(alarmSystemStatus,off)() {
+	log.debug "Executing 'sendEvent(alarmSystemStatus,off)'"
+	// TODO: handle 'sendEvent(alarmSystemStatus,off)' command
+}
+
+def sendEvent(alarmSystemStatus,stay)() {
+	log.debug "Executing 'sendEvent(alarmSystemStatus,stay)'"
+	// TODO: handle 'sendEvent(alarmSystemStatus,stay)' command
+}
+
+def sendEvent(alarmSystemStatus,away)() {
+	log.debug "Executing 'sendEvent(alarmSystemStatus,away)'"
+	// TODO: handle 'sendEvent(alarmSystemStatus,away)' command
+}
+
+def capture() {
+	log.debug "Executing 'capture'"
+	// TODO: handle 'capture' command
+}
+
+def open() {
+	log.debug "Executing 'open'"
+	// TODO: handle 'open' command
+}
+
+def close() {
+	log.debug "Executing 'close'"
+	// TODO: handle 'close' command
+}
+
+def armStay() {
+	log.debug "Executing 'armStay'"
+	// TODO: handle 'armStay' command
+}
+
+def armAway() {
+	log.debug "Executing 'armAway'"
+	// TODO: handle 'armAway' command
+}
+
+def disarm() {
+	log.debug "Executing 'disarm'"
+	// TODO: handle 'disarm' command
+}
